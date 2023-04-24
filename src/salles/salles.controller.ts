f8186ee33261ca888b6e0f1f1995ff10d395b07f
SANFILIPPOJF <jean-francois.sanfilippo@sncf.fr>
2023-04-24 12:05:16 +0200

    body: name salles

--- src/salles/salles.controller.ts
+++ src/salles/salles.controller.ts
@@ -7,30 +7,34 @@ import { TransformInterceptor } from 'src/interceptor/TransformInterceptor';
 
 @Controller('salles')
 @UseInterceptors(TransformInterceptor) // transforme toutes les responses avec statusCode, status et data
-@ApiTags('SALLES') // cree une categorie USERS dans swagger UI
+@ApiTags('SALLES') // cree une categorie SALLES dans swagger UI
 export class SallesController {
-  constructor(private readonly sallesService: SallesService) {}
+  constructor(private readonly sallesService: SallesService) { }
 
   @Post()
   async create(@Body() createSalleDto: CreateSalleDto) {
+    if (createSalleDto.capacite < 1 || Math.floor(createSalleDto.capacite) !== createSalleDto.capacite)
+      throw new HttpException('Capacite must be a positive integer', HttpStatus.BAD_REQUEST);
     const sameSalle = await this.sallesService.findOneByName(createSalleDto.name);
     if (!sameSalle) return this.sallesService.create(createSalleDto);
     if (sameSalle.isActive) throw new HttpException('Salle allready exist', HttpStatus.CONFLICT);
-    return this.sallesService.active(sameSalle,createSalleDto);
+    return this.sallesService.active(sameSalle, createSalleDto);
   }
 
   @Get()
   async findAll() {
     const salles = await this.sallesService.findAll();
-    if (salles.length===0) throw new HttpException('No salle found', HttpStatus.NOT_FOUND);
+    if (salles.length === 0) throw new HttpException('No salle found', HttpStatus.NOT_FOUND);
     return salles
   }
 
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateSalleDto: UpdateSalleDto) {
-    if (isNaN(+id) || +id < 1 || Math.floor(+id)!==+id) throw new HttpException('ID must be a positive integer', HttpStatus.BAD_REQUEST);
+    if (isNaN(+id) || +id < 1 || Math.floor(+id) !== +id) throw new HttpException('ID must be a positive integer', HttpStatus.BAD_REQUEST);
     if (!updateSalleDto.adresse && !updateSalleDto.capacite && !updateSalleDto.name)
       throw new HttpException('nothing to update', HttpStatus.BAD_REQUEST);
+    if (updateSalleDto.capacite && (updateSalleDto.capacite < 1 || Math.floor(updateSalleDto.capacite) !== updateSalleDto.capacite))
+      throw new HttpException('Capacite must be a positive integer', HttpStatus.BAD_REQUEST);
     const salleFound = await this.sallesService.findOne(+id);
     if (!salleFound) throw new HttpException('salle not found', HttpStatus.NOT_FOUND);
     if (!salleFound.isActive) throw new HttpException('salle deleted', HttpStatus.NOT_FOUND);
@@ -39,7 +43,7 @@ export class SallesController {
 
   @Delete(':id')
   async remove(@Param('id') id: string) {
-    if (isNaN(+id) || +id < 1 || Math.floor(+id)!==+id) throw new HttpException('ID must be a positive integer', HttpStatus.BAD_REQUEST);
+    if (isNaN(+id) || +id < 1 || Math.floor(+id) !== +id) throw new HttpException('ID must be a positive integer', HttpStatus.BAD_REQUEST);
     const salleFound = await this.sallesService.findOne(+id);
     if (!salleFound) throw new HttpException('salle not found', HttpStatus.NOT_FOUND);
     if (!salleFound.isActive) throw new HttpException('salle already deleted', HttpStatus.BAD_REQUEST);
